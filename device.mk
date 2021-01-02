92ff4b5c3b5a01403dd58dfaa816230bcc28ff4d
Lup Gabriel <gwolf2u.website@gmail.com>
2021-01-02 19:47:21 +0200

    hotdogb: add FOD animations
    Change-Id: I440e3ac778ea2c8af33855391c1ae3df75ebe8d4

--- device.mk
+++ device.mk
@@ -30,6 +30,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/odm_feature_list:$(TARGET_COPY_OUT_ODM)/etc/odm_feature_list
 
+# FOD
+EXTRA_FOD_ANIMATIONS := true
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
